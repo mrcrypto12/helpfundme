7d172182c9cdb7f5eae93c66d809d5cdc4fca36c
mrcrypto12 <lovedanie264@gmail.com>
2026-09-18 20:56:26 +0000

    done

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -23,9 +23,9 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/api/auth/google/callback',
+        clientID: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL!,
       },
       async (
         accessToken: string,
