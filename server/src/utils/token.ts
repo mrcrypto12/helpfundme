452f7d3fa130219310a8c343c147b510ad304d72
mrcrypto12 <lovedanie264@gmail.com>
2026-09-18 19:47:44 +0000

    done

--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -1,19 +1,25 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import { IUser } from '../models/User';
 
 export const generateAccessToken = (user: IUser): string => {
+  const options: SignOptions = {
+    expiresIn: (process.env.JWT_ACCESS_EXPIRES_IN || '15m') as SignOptions['expiresIn'],
+  };
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_ACCESS_SECRET as string,
-    { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m' }
+    options
   );
 };
 
 export const generateRefreshToken = (user: IUser): string => {
+  const options: SignOptions = {
+    expiresIn: (process.env.JWT_REFRESH_EXPIRES_IN || '7d') as SignOptions['expiresIn'],
+  };
   return jwt.sign(
     { id: user._id },
     process.env.JWT_REFRESH_SECRET as string,
-    { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d' }
+    options
   );
 };
 
